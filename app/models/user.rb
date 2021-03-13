55d37c9eef5f1057c2b5258dbc863d3aa558cf4e
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-13 12:37:33 -0500

    commit tree 98ae338b7f94d416ff8a1d5b68417f0c9f09aad1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, through: :listings, :foreign_key => 'host_id'
 
-  has_many :reservations, :foreign_key => 'guest_id'
   has_many :reviews, :foreign_key => 'guest_id'
+  # has_many :trips, through: :listings, :source => 'reservations'
+  has_many :trips, :class_name => "Reservation", foreign_key: 'guest_id' 
+  # it seems like this is a way of renaming reservations, since you've already used it above?
 end
   
