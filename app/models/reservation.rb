d07d63aaa695a0fd636885a714162f3c31c08b1e
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-13 12:12:13 -0500

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,7 @@
 class Reservation < ApplicationRecord
-  belongs_to :guest, :class_name => "User"
-  # belongs_to :host, :class_name => "User"
-  belongs_to :listing
   has_many :reviews
+  belongs_to :listing
+
+  belongs_to :guest, :class_name => "User"
+  
 end
